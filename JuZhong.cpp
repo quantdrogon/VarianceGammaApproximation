b35bf4af21cbb7384bdbb5cf77b69c220eba1c44
amirosko <amir.osko@gmail.com>
2017-10-31 16:33:30 -0400

    Add files via upload

--- JuZhong.cpp
+++ JuZhong.cpp
@@ -1,8 +1,6 @@
 #include "JuZhong.hpp"
 
 
-
-
 double JuZhongOption::findSxViaNewton(double initialGuess, double K, double r, double q, double sig, double T, double europeanPremium, double lambdaH, std::string PutCall, double maxIterNewton, double tol)
 {
     int counter=0;
@@ -79,9 +77,30 @@ void JuZhongOption::JuZhongPricer()
     
     }
 }
+ double JuZhongOption::JuZhongOption(double S, double K, double r, double q, double sig, double T, double europeanPremium, double deltaE, double gammaE, double vegaE, double thetaE, std::string PutCall, int maxIterNewton, double tol): : S(S), K(K), r(r), q(q), sig(sig), T(T), europeanPremium(euroPremium), deltaE(deltaE), gammaE(gammaE), vegaE(vegaE), thetaE(thetaE), lambdaH(lambdaH), PutCall(PutCall), maxIterNewton(maxIterNewton), tol(tol);
+{
+        alpha = 2 * r/(sig^2);
+        beta = 2 * (r-q)/(pow(sig,2);
+        hTau = 1 - exp(-r*T);
+        lambdaH = (-(beta-1) + phi * sqrt(pow(beta-1,2) + 4 * alpha/hTau))/2;
     
+        qInfty =  (1 - beta + phi * sqrt(pow(beta-1,2) + 4*alpha))/2;
+        sInfty = K/(1 - 1/qInfty);
+        hi = (-phi*(r-q)*T - 2*sig*sqrt(T)) * K / (phi * (sInfty - K));
+        initialGuess = sInfty + (K - sInfty) * exp(hi);
+
+        Sx = findSxViaJuZhong(initialGuess, K, r, q, sig, T, phi, lambdaH);
+        ah = (phi * (Sx - K) - europeanPremium)/hTau;
+        //ah = (phi * (Sx - K) - BlackScholesEuropeanOption(Sx, K, r, q, sig, T, europeanPremium, lambdaH, PutCall)
+        
+    //NOTE: thetaE = BlackScholesEuropeanOption(Sx, K, r, q, sig, T, phi=phi, greekCal=True)[-1]
+    //check if we can do this for theta
+        lambdaHDerivation = -phi * alpha / (hTau^2 * sqrt(pow(beta-1,2) + 4*alpha/hTau));
+        b = (1 - hTau) * alpha * lambdaHDerivation/(2*(2 * lambdaH + beta - 1));
+        c = - (1 - hTau) * alpha / (2 * lambdaH + beta - 1) * (-thetaE/(hTau * ah * r * exp(-r*T)) + 1/hTau + lambdaHDerivation/(2 * lambdaH + beta - 1));
+        JuZhongPricer();
+}   
 double JuZhongOption::getPremium()
 {
     return AmerPrice;
 }
-
