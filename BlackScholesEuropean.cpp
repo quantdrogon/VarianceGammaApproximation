da4ef4592599d0271b8bced947109b3bfb34079c
Abhishek Sanghani <as5198@columbia.edu>
2018-01-04 16:57:22 +0530

    Update BlackScholesEuropean.cpp

--- BlackScholesEuropean.cpp
+++ BlackScholesEuropean.cpp
@@ -18,25 +18,27 @@ void BlackScholesEuropeanOption::calculateValues()
     double b1 = exp(-q*T);
     double b2 = exp(-r*T);
     
-    gamma = b1*cdf(Z,d1)/(s*bottom);
-    vega = s*b1*pdf(Z,d1)/100;
+    gamma = b1*norm_cdf(d1)/(s*bottom);
+    vega = s*b1*norm_pdf(d1)/100;
     
     if (optionType == "C")
     {
-        double nd1 = cdf(Z,d1);
-        double nd2 = cdf(Z,d2);
+        double nd1 = norm_cdf(d1);
+        double nd2 = norm_cdf(d2);
         premium = s*b1*nd1 - K*b2*nd2;
         delta = b1*nd1;
-        theta = -(b1*s*pdf(Z,d1)*v/(2*bottom)) - r*K*b2*cdf(Z,d2)+q*s*b1*cdf(Z,d1);
+        theta = -(b1*s*norm_pdf(d1)*v/(2*bottom)) - r*K*b2*norm_cdf(d2)+q*s*b1*norm_cdf(d1);
     }
     else if(optionType == "P")
     {
-        double nNd1 = cdf(Z,-d1);
-        double nNd2 = cdf(Z,-d2);
+        double nNd1 = norm_cdf(-d1);
+        double nNd2 = norm_cdf(-d2);
         premium = K*b2*nNd2 - s*b1*nNd1;
         delta = -b1*nNd2;
-        theta = -(b1*s*pdf(Z,d1)*v/(2*bottom)) + r*K*b2*cdf(Z,-d2) - q*s*b1*cdf(Z,-d1);
+        theta = -(b1*s*norm_pdf(d1)*v/(2*bottom)) + r*K*b2*norm_cdf(-d2) - q*s*b1*norm_cdf(-d1);
     }
+    
+    
     
 }
 
